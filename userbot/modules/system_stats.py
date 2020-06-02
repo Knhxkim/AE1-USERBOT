04e4e0b9a7c9fcc8d73ab4a036913a09e28a346a
THEALIFHAKER1 <56091627+THEALIFHAKER1@users.noreply.github.com>
2020-06-01 17:21:14 -0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -133,16 +133,15 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit(
-                     "`i am ᗩᒪᓰᐺᘿ My 𝕄𝕒𝕤𝕥𝕖𝕣` \n"
-                     "`𝘪 𝙘𝙖𝙣'𝙩 Ðïê` \n"
-                     f"тєℓєтнση νєяѕιση: {version.__version__} \n"
-                     f"P̳y̳t̳h̳o̳n̳ ̳v̳e̳r̳s̳i̳o̳n̳: {python_version()} \n"
+                     "`I ALIVE MY MASTER` \n"
+                     "`I WILL SERVE YOU EVERYTHING` \n"
+                     f"TELETHON VERSION: {version.__version__} \n"
+                     f"PYTHON VERSION: {python_version()} \n"
                      f"------------------------------------ \n"
-                     f"ᗯEᗷᔕITE: 𝖍𝖙𝖙𝖕𝖘://𝖜𝖜𝖜.𝖋𝖆𝖈𝖊𝖇𝖔𝖔𝖐.𝖈𝖔𝖒/𝕿𝖊𝖐𝖓𝖔𝖜𝖆𝖞𝖘 \n"
-                     f"U̴̧̡̫̤̦̇͆͛̿͑̈́̂̊̚͝s̷̡͓͎͘e̷̹̙̝̽̾͂ŕ̴̡̛̺̖̝̬̣͖͕̐̅͌͂͌̕:: {DEFAULTUSER} \n"
-                     f"Mαιɳƚαιɳҽɾ: @🄼🄰🅈🅄🅁_🄺🄰🅁🄰🄽🄸🅈🄰 \n"
-                     f"🅰🅳🅼🅸🅽: `@𝓣𝓱𝓻𝓮𝓮_𝓒𝓾𝓫𝓮_𝓣𝓮𝓚𝓷𝓸𝔀𝓪𝔂𝓼` \n"
-                     f"U҉s҉e҉r҉b҉o҉t҉: @ₜₑₛₜing_bₒₜ "
+                     f"WEBSIDE:  \n"
+                     f"USER: {DEFAULTUSER} \n"
+                     f"MAINTAINER: `@THEALIFHAKER1` \n"
+                     f"#AE1_TEAM "
                      )    
 
 
