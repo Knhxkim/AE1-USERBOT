c14090d0c8e1a455eca4d70b7bcefd81dec7c99d
THEALIFHAKER1 <56091627+THEALIFHAKER1@users.noreply.github.com>
2020-06-09 01:12:42 +0800

    Update follow.py

--- userbot/modules/follow.py
+++ userbot/modules/follow.py
@@ -1,7 +1,4 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
+# Copyright (C) 2020 AE1™-USERBOT
 #
 """ Userbot module for getting information about the social media. """
 
@@ -33,10 +30,11 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def follow(follow):
     """ For .follow command, check if the bot is running.  """
     await follow.edit(
-                     f"`FOLLOW {DEFAULTUSER} ON` \n\n"
-                     f"[InstaGram](https://www.instagram.com/mayur_karaniya) \n\n"
-                     f"[FaceBook](https://www.facebook.com/mkaraniya) \n\n"
-                     f"[YouTube](https://www.youtube.com/channel/UCeKQxQK7XZ3jGi3541uWATg?sub_confirmation=1) "
+                     f"`FOLLOW @THEALIFHAKER1 ON` \n\n"
+                     f"[InstaGram](https://www.instagram.com/thealifhaker1) \n\n"
+                     f"[Telegram](t.me/THEALIFHAKER1) \n\n"
+                     f"[YouTube](https://www.youtube.com/channel/UCw7qTq7f--Ft2ONNI1cpiVw) \n\n"
+                     f"[Paypal](https://www.paypal.me/AEALIFDANIEL2003) \n\n"
                      )    
 
 
